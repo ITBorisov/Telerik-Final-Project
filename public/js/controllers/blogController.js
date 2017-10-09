a48e203450c0114f0e5b59a547382c3a4f7f00a3
Georgi <g.borisov123@gmail.com>
2017-10-10 00:36:13 +0300

    change in responsive and add a tag in blog.path

--- public/js/controllers/blogController.js
+++ public/js/controllers/blogController.js
@@ -6,15 +6,15 @@ import { getReviews, getLatestUsers, getLatestReview } from 'data';
 export function blogController (context){
     Promise.all([getReviews('reviews'), getLatestUsers(6), getLatestReview('reviews', 6), loadTemplate('blog') ])
         .then(([dataResponse, newestUsers, latestReview, template]) => {
-            console.log(newestUsers);
+            
             let reviews = {
 				review: dataResponse,
                 users: newestUsers,
                 newReviews: latestReview
 			};
-
+            console.log(reviews);
             context.$element().html(template(reviews));
 			$(".preloading").fadeOut("slow");
-        })   
+        });
     
 }
